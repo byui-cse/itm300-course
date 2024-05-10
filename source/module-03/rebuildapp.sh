fdf16661d7c9f8273db1e607b62870b0929a08e1
AHFXAdam <adam@ahfx.net>
2024-05-10 13:24:58 -0600

    updated build script

--- source/module-03/rebuildapp.sh
+++ source/module-03/rebuildapp.sh
@@ -8,4 +8,27 @@ unzip quick-oil-part3.zip -d /tmp/lab-app
 mv /tmp/lab-app/quick-oil-part3/* /var/www/html
 rm -rf /tmp/lab-app/
 rm quick-oil-part3.zip
+
+echo "Please enter your User Pool ID:"
+read user_pool_id
+
+# Define the file path of login.js
+file_path="/var/www/html/scripts/login.js"
+
+# Use sed to replace the line containing 'UserPoolId: REPLACE-WITH-YOUR-USER-POOL-ID'
+sed -i "s/UserPoolId: 'REPLACE-WITH-YOUR-USER-POOL-ID',/UserPoolId: '$user_pool_id',/" "$file_path"
+
+echo "User Pool ID updated in $file_path"
+
+echo "Please enter your User Client ID:"
+read user_client_id
+
+# Define the file path of login.js
+file_path="/var/www/html/scripts/login.js"
+
+# Use sed to replace the line containing 'UserPoolId: REPLACE-WITH-YOUR-USER-POOL-ID'
+sed -i "s/ClientId: 'REPLACE-WITH-YOUR-CLIENT-ID'/ClientId: '$user_client_id'/" "$file_path"
+
+echo "User Client ID updated in $file_path"
+
 rm $0
