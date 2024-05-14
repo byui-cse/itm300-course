58c565573981deaaf4e0fd661ad71e354ba4af4b
AHFXAdam <adam@ahfx.net>
2024-05-14 17:07:25 -0600

    fix build script lab 6

--- docs/module-06/rebuildapp.sh
+++ docs/module-06/rebuildapp.sh
@@ -3,15 +3,15 @@
 sudo dnf install -y wget
 wget https://github.com/byui-cse/itm300-course/raw/main/source/module-06/quick-oil-part5.zip
 mkdir /tmp/lab-app
-unzip quick-oil-part4.zip -d /tmp/lab-app
+unzip quick-oil-part5.zip -d /tmp/lab-app
 # Remove so that we don't overwrite variables they have stored
-rm /tmp/lab-app/quick-oil-part4/scripts/login.js
-rm /tmp/lab-app/quick-oil-part4/scripts/getMessage.js
+rm /tmp/lab-app/quick-oil-part5/scripts/login.js
+rm /tmp/lab-app/quick-oil-part5/scripts/getMessage.js
 
-mv /tmp/lab-app/quick-oil-part4/* /var/www/html
-mv /tmp/lab-app/quick-oil-part4/scripts/* /var/www/html/scripts
+mv /tmp/lab-app/quick-oil-part5/* /var/www/html
+mv /tmp/lab-app/quick-oil-part5/scripts/* /var/www/html/scripts
 rm -rf /tmp/lab-app/
-rm quick-oil-part4.zip
+rm quick-oil-part5.zip
 
 ######################################################
 
