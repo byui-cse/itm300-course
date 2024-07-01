52e89a78be74ed0811709c333230e4b612be7a59
AHFXAdam <adam@ahfx.net>
2024-07-01 14:18:07 -0600

    update labs and rebuild script

--- docs/module-07/rebuildapp.sh
+++ docs/module-07/rebuildapp.sh
@@ -4,6 +4,8 @@ sudo dnf install -y wget
 wget https://github.com/byui-cse/itm300-course/raw/main/source/module-07/quick-oil-part6.zip
 mkdir /tmp/lab-app
 unzip quick-oil-part6.zip -d /tmp/lab-app
+rm /tmp/lab-app/quick-oil-part7/scripts/settings.js
+
 # Remove so that we don't overwrite variables they have stored
 mkdir -p /var/www/html
 cp -rf /tmp/lab-app/quick-oil-part6/* /var/www/html/
